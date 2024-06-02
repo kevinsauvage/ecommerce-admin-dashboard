bb5ed647129aa9e4a65ff3861baa75c5fc949ccf
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2024-06-02 20:35:23 +0200

    remove unique constraint on store name

--- src/actions/storeActions.ts
+++ src/actions/storeActions.ts
@@ -1,6 +1,5 @@
 'use server';
 
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
