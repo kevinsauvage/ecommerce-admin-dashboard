f0bdb87dc2c71f4f1cefe2964899667ffeb50c8f
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2024-06-11 22:15:44 +0200

    refactor

--- src/actions/_utils/isAuthorized.ts
+++ src/actions/_utils/isAuthorized.ts
@@ -4,8 +4,5 @@ import { getSession } from '@/lib/auth';
 
 export default async function isAuthorized(storeId: string) {
   const session = await getSession();
-
-  if (!session || !storeId) {
-    return redirect('/login');
-  }
+  if (!session || !storeId) return redirect('/login');
 }
